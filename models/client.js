9b236b04ad2010ddcbfc1e23cc5f9a87b5a8e4da
HATEM-CHAABINI <hatem.chaabini@esprit.tn>
2020-12-21 23:11:51 +0100

    Update client.js

--- models/client.js
+++ models/client.js
@@ -2,10 +2,10 @@ var mongoose = require("mongoose");
 
 var clientSchema = new mongoose.Schema({
   nom: String,
-  email: String,
   numTel: String,
   metier: String,
   ville: String,
+  commentaire: String,
   images: [String] ,
   longitude: Number,
   latitude: Number,
