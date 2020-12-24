3b33fa5ae698738b9269cede585e176a5b129ff9
HATEM_CHAABINI <hatem.chaabini@esprit.tn>
2020-12-24 22:28:41 +0100

    sql

--- services/clientService.js
+++ services/clientService.js
@@ -1,3 +1,13 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'database',
+  port: 8889
+});
+ 
 var client = require("../models/client");
 
 exports.getClient = async function (query, page, limit) {
@@ -19,16 +29,40 @@ exports.getClientByid = async function (id) {
 exports.addNewClient = async function (document) {
   try {
     images = [];
+    var im ="";
+    console.log("im");
+    console.log(document.body);
+    console.log("im");
     for (const file of document.files) {
       console.log("file.filename");
       console.log(file.filename);
       console.log("file.filename");
       images.push(file.filename);
+      im = im + file.filename +" , "
     }
-    console.log(images);
+    im = im.slice(0, -2)
+    console.log("im");
+    console.log(im);
+    console.log("im");
+
     document.body.images = images;
     var content = await client.create(document.body);
+    var post_data = document.body;
+    var name= post_data.nom;
+    var numero= post_data.numTel;
+    var subject_position= post_data.ville;
+    var subject_state= post_data.metier;
+    var message= post_data.commentaire;
+    var lon= post_data.longitude;
+    var lat= post_data.latitude;
 
+
+    con.query('INSERT INTO `reclame`(`name`,`numero`, `subject_position`, `subject_state`, `file_name`, `message`,`lon`, `lat`, `upload_time`) VALUES (?,?,?,?,?,?,?,?,NOW())',[name,numero,subject_position,subject_state,im,message,lon,lat],function(err,result,fields){
+      con.on('error',function(err){
+      console.log('[MySQL ERROR]',err);
+    });
+
+    })
     return content;
   } catch (e) {
     throw Error("Error while creating new client");
